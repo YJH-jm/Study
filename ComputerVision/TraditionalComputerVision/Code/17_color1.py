f77e83cea5c7b836c3dba2d8787eac52d27f5a08
YJH-jm <jmjhjob13@gmail.com>
2022-04-14 20:20:52 +0900

    UPDATE: "cv2.cvtColor"

--- ComputerVision/TraditionalComputerVision/Code/17_color1.py
+++ ComputerVision/TraditionalComputerVision/Code/17_color1.py
@@ -9,16 +9,25 @@ if src is None:
     print("Image load failed")
     sys.exit()
 
+
 # 컬러 영상 속성 확인
 print("src.shape :", src.shape) # src.shape : (480, 640, 3)
 print("src.dtype : ", src.dtype) # src.dtype :  uint8
 
 planes = cv2.split(src)
-cv2.imshow("src", src)
+# cv2.imshow("src", src)
+# cv2.imshow("planes[0]", planes[0])
+# cv2.imshow("planes[1]", planes[1])
+# cv2.imshow("planes[2]", planes[2])
+
+
+# HSV로 변환
+src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+planes = cv2.split(src_hsv)
+cv2.imshow("src_hsv", src_hsv)
 cv2.imshow("planes[0]", planes[0])
 cv2.imshow("planes[1]", planes[1])
 cv2.imshow("planes[2]", planes[2])
-
 
 
 cv2.waitKey()
